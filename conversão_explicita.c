4bf2373cbf42a1dd216131705da93e0a5b3154a4
Denisonfaust <denison.faust@gmail.com>
2025-08-16 14:38:29 +0000

    conversão explicita

---
+++ conversão_explicita.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+
+//Declaração das variáveis
+
+int a = 10;
+int b = 3;
+float quociente = (float) a / b; // 'a' é explicitamente convertido para float
+
+printf("Resultado: %.2f \n", quociente);
+
+return 0;
+
+
+}
