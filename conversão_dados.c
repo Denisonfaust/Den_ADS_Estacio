49383e3f9fc416454e2fb83d411d2126f865e051
Denisonfaust <denison.faust@gmail.com>
2025-08-16 14:21:20 +0000

    Conversão_implicita

---
+++ conversão_dados.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+
+//Declaração das variáveis
+
+int a = 10;
+float b = 3.5;
+float resultado = a + b; // 'a' é convertido implicitamente para float
+
+printf("Resultado: %.2f \n", resultado);
+
+return 0;
+
+
+}
