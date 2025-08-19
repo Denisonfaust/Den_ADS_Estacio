cede1a3218702134d5c09afe7c7e0b01a063ca84
Denisonfaust <denison.faust@gmail.com>
2025-08-19 21:09:33 +0000

    criei o else

--- nota_teste_if.c
+++ nota_teste_if.c
@@ -3,16 +3,13 @@
 int main () {
 
 int idade1 = 71;
-int idade2 = 70;
+int idade2 = 72;
 
-if(idade1 != idade2) {
-printf("As pessoas tem idade diferente! \n");
-
+if(idade1 > idade2) {
+printf("A pessoas 1 é mais velha! \n");
+}else {
+printf("A pessoa 2 é mais velha \n ");
 }
-
-
-
-
 
 
 
