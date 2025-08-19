bae778062865df32737af29b4940c5a9ffcc8cf4
Denisonfaust <denison.faust@gmail.com>
2025-08-19 19:36:55 +0000

    aplicação do if

---
+++ teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int num;
+
+    printf("entre com o numero: \n");
+    scanf("%d", &num);    
+
+    num = num + num % 3;
+
+    printf("num = %d\n", num);
+    
+    return 0;
+}
