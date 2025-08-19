b7d0344051385b512a8014bf4f012b5fa30b6113
Denisonfaust <denison.faust@gmail.com>
2025-08-19 20:49:29 +0000

    criei com else

--- teste_operador%.c
+++ teste_operador%.c
@@ -2,16 +2,21 @@
 
 int main () {
 
-int numero = 5, resultado;
+int numero = 11, resultado;
 
 resultado = numero % 2;
 
   printf("A variavel resultado é: %d \n", resultado);
 
-if(resultado != 0) {
-  printf("O número é impar \n");
+if(resultado == 0) {
+  printf("O número é par \n");
+}
+else {
+  printf("O numero é impar \n");
 
 }
+
+
 
 
 
