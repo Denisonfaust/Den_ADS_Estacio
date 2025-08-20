a24219142975b9a931c7f2a00b5ea86e78fbe2d5
Denisonfaust <denison.faust@gmail.com>
2025-08-20 21:07:39 +0000

    invertido as notas do programa

--- estrutura_encadeadas2.c
+++ estrutura_encadeadas2.c
@@ -13,18 +13,18 @@ int main() {
 // D >= 60
 // E >= 50
 
-    if(nota > 90){
-    printf("O conceito é A \n");
-    } else if(nota >= 80){
-    printf("O conceito é B \n");
-    } else if(nota >= 70){
-    printf("O conceito é C \n");
-    } else if (nota >= 60){
-    printf("O conceito é D \n");
-    } else if(nota >= 50){
-    printf("O conceito é E \n");
-    } else{
+    if(nota < 50){
     printf("O conceito é F \n");
+    } else if(nota <= 60){
+    printf("O conceito é E \n");
+    } else if(nota <= 70){
+    printf("O conceito é D \n");
+    } else if (nota <= 80){
+    printf("O conceito é C \n");
+    } else if(nota <= 90){
+    printf("O conceito é B \n");
+    } else{
+    printf("O conceito é A \n");
     }
 
 }
