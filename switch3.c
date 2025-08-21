e07212b7d3473bf639d946a9a44fb82f116bf93c
Denisonfaust <denison.faust@gmail.com>
2025-08-21 17:16:31 +0000

    encadeada x switch

--- switch3.c
+++ switch3.c
@@ -28,11 +28,33 @@ int main() {
         printf("Sábado\n");
     }
 
-    
+    switch (dia)
+    {
+    case 1:
+        printf("Domingo_s\n");
+        break;
+    case 2:
+        printf("Segunda feira_s\n");
+        break;
+    case 3:
+        printf("Terça feira_s\n");
+        break;
+    case 4:
+        printf("Quarta feira_s\n");
+        break;
+    case 5:
+        printf("Quinta feira_s\n");
+        break;
+    case 6:
+        printf("Sexta feira_s\n");
+        break;
+    case 7:
+        printf("Sábado_s\n");
+        break;
+    default:
+        printf("Dia inválio_s");
+        }
 
-
-
-
-
+    return 0;
 
 }
