13781fa3222002296dfe1fe6d14a30d30fd4d388
Denisonfaust <denison.faust@gmail.com>
2025-08-21 16:58:12 +0000

    encadeada if-else x switch

---
+++ switch3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int dia;
+
+    printf("Digite o dia: \n");
+    scanf("%d", &dia);
+
+    if(dia == 1){
+        printf("Domingo\n");
+    } else if(dia == 2)
+    {
+        printf("segunda feira\n");
+    } else if(dia == 3)
+    {
+        printf("Terça feira\n");
+    } else if(dia == 4)
+    {
+        printf("Quarta feira\n");
+    }else if(dia == 5)
+    {
+        printf("Quinta feira\n");
+    }else if(dia == 6)
+    {
+        printf("Sexta feira\n");
+    }else {
+        printf("Sábado\n");
+    }
+
+    
+
+
+
+
+
+
+}
