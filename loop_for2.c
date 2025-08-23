5ebd799ff1496163036b0783c975856534e3473a
Denisonfaust <denison.faust@gmail.com>
2025-08-23 15:06:45 +0000

    for com decremento

--- loop_for2.c
+++ loop_for2.c
@@ -3,6 +3,9 @@
 //Exemplo de utilização de loop for com if 
 int main() {
     for (int i = 1; i <= 20; i++) {
+/* Nesta condição o programa vai listar somente os números ímpares, 
+pq a condição é o resto da divisão de i é diferente de 0*, se o resto da divisão fosse = 0 
+0, o programa listaria somente os números pares*/
         if (i % 2 != 0) {
             printf("%d\n", i);
         }
