993479911581a891978d94b27ecfab3c59b2125f
Denisonfaust <denison.faust@gmail.com>
2025-08-23 15:07:40 +0000

    loop com decremento

---
+++ loop_for3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Exemplo de loop for com decremento de 10 em 10
+int main() {
+    for (int i = 100; i >= 0; i -= 10) {
+        printf("%d\n", i);
+    }
+   
+    return 0;
+}
