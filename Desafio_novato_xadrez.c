81a39ecc346a80185d6ebfe89457ca5e11f85ac4
Denisonfaust <denison.faust@gmail.com>
2025-08-23 23:16:28 +0000

    Movimento da torre

---
+++ Desafio_novato_xadrez.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+//Mover a torre 5 casas para direita
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Torre para Direita\n");//Imprime a direção do monimento
+    }
+    return 0;
+}
