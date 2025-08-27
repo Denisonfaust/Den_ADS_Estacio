b855a17e8989cb6b7fe7dc10c196ad93da880116
Denisonfaust <denison.faust@gmail.com>
2025-08-27 12:04:57 +0000

    Desafio_Novato_Batalha_Naval

--- Desafio_Novato_Batalha_Naval.c
+++ Desafio_Novato_Batalha_Naval.c
@@ -4,24 +4,33 @@ int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 char coluna[10] = {'A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
 int main() {
-    
     int i, j;
-    
-    //Imprimindo o cabeçalho da linha
-    printf("  ");
+
+    // Cabeçalho das colunas
+    printf("   ");
     for(j = 0; j < 10; j++){
         printf(" %c ", coluna[j]);
     }
     printf("\n");
 
-    //Imprimindo a matriz com as linhas e colunas
+    // Impressão da matriz
     for(i = 0; i < 10; i++){
-        printf("%d ", linha[i]);
+        printf("%2d ", linha[i]);  // Alinhamento correto
         for(j = 0; j < 10; j++){
-            printf(" 0 ");//Imprimindo o valor 0 em cada posição da matriz
+            // Verifica E3, F3, G3
+            if(i == 2 && (j == 4 || j == 5 || j == 6)){
+                printf(" 3 ");
+            }
+            // Verifica H5, H6, H7
+            else if(j == 7 && (i == 4 || i == 5 || i == 6)){
+                printf(" 3 ");
+            }
+            else {
+                printf(" 0 ");
+            }
         }
         printf("\n");
     }
+
     return 0;
-       
 }
