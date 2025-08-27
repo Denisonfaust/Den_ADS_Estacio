58f9fa3907ad03cb3489b3b8910cef311d63ae2f
Denisonfaust <denison.faust@gmail.com>
2025-08-27 23:10:23 +0000

    Desafio_Batalha_Naval_Aventureiro

---
+++ Desafio_Batalha_Naval_Aventureiro.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+int main() {
+    int i, j;
+
+    // Cabeçalho das colunas
+    printf("   ");
+    for(j = 0; j < 10; j++){
+        printf(" %c ", coluna[j]);
+    }
+    printf("\n");
+
+    // Impressão da matriz
+    for(i = 0; i < 10; i++){
+        printf("%2d ", linha[i]);  // Alinhamento correto
+        for(j = 0; j < 10; j++){
+            // Imprime A8, B7, C6
+            if((i == 7 && j == 0) || (i == 6 && j == 1) || (i == 5 && j == 2)) {
+                printf(" 3 ");
+            }
+            // Imprime E8, F9, G10
+            else if((i == 7 && j == 4) || (i == 8 && j == 5) || (i == 9 && j == 6)) {
+                printf(" 3 ");
+            }
+            // Imprime E3, F3, G3
+            else if(i == 2 && (j == 4 || j == 5 || j == 6)){
+                printf(" 3 ");
+            }
+            // Imprime H5, H6, H7
+            else if(j == 7 && (i == 4 || i == 5 || i == 6)){
+                printf(" 3 ");
+            }
+            else {
+                printf(" 0 ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
